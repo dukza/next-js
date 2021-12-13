8e29a4b2c12c4de38f17469e5a18de8d02fd70ac
ha duk yeon <hadukyeon@haui-MacBookPro.local>
2021-12-13 14:37:07 +0900

    Deploy Next.js to gh-pages

--- _next/static/H5hIx7WHiDgcoTKZF3k3p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/282-f36b495573221875.js","static/chunks/pages/index-41333c4376101cfb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/PyiH7AfBGxF45jW1ziWta/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/282-6c4546767bc13502.js","static/chunks/pages/index-1220fd27ac113ca9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
